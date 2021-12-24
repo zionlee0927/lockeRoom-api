78934d16d009d870c85a0ec1ae36f7025635a7fa
zionlee0927 <zionlee0927@tpirates.com>
2021-12-24 17:07:52 +0900

    feat: HelloController add

---
+++ src/main/java/com/lockeroom/api/hello/HelloController.java
@@ -0,0 +1,14 @@
+package com.lockeroom.api.hello;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/heartbeat")
+    public ResponseEntity heartbeatCheck(){
+        return ResponseEntity.ok().body("hello");
+    }
+}
---
+++ src/test/java/com/lockeroom/api/hello/HelloControllerTest.java
@@ -0,0 +1,25 @@
+package com.lockeroom.api.hello;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+class HelloControllerTest {
+
+    private HelloController helloController = new HelloController();
+
+    private MockMvc mockMvc = MockMvcBuilders.standaloneSetup(helloController).build();
+
+    @Test
+    @DisplayName("say hello")
+    void heartbeat() throws Exception {
+        mockMvc.perform(get("/heartbeat"))
+                .andExpect(content().string("hello"))
+                .andExpect(status().isOk());
+    }
+}
