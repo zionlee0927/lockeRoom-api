8411d7f79a4b61b3f1c2bfa5a3725f8ec071eb23
zionlee0927 <zionlee0927@tpirates.com>
2021-12-24 10:26:00 +0900

    Initial commit

---
+++ src/main/java/com/lockeroom/api/LockeRoomApplication.java
@@ -0,0 +1,13 @@
+package com.lockeroom.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockeRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LockeRoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lockeroom/api/LockeRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lockeroom.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockeRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
